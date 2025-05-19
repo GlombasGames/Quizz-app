936cd4524204397cb96c4873c841197eaaee25f3
Victor Glomba <victorglomba@gmail.com>
2025-05-19 17:54:29 -0300

    2

--- public/main.js
+++ public/main.js
@@ -248,17 +248,17 @@ function renderMenu() {
 
   app.innerHTML = `
     <div class="header">
-    <div class="header-item">
-        <p><img src="./assets/coin.png" alt="coin"> ${progreso.intentos}</p>
-        </div>
-        <div class="header-item">
-        <button class="btn-anuncio btn-anuncio-header" tabindex="0" onclick="verAnuncio()" ${botonAnuncioDisabled ? 'disabled' : ''}>
-          <p>Ver AD <img src="./assets/coin.png" alt="coin"> +1</p>       
-        </button>
-        </div>
-        <div class="header-item">
-          ${totalPuntos} pts
-        </div>
+     <div class="header-item">
+         <p><img src="./assets/coin.png" alt="coin"> ${progreso.intentos}</p>
+     </div>
+     <div class="header-item">
+      <button class="btn-anuncio btn-anuncio-header" tabindex="0" onclick="verAnuncio()" ${botonAnuncioDisabled ? 'disabled' : ''}>
+       <p>Ver AD <img src="./assets/coin.png" alt="coin"> +1</p>       
+      </button>
+     </div>
+     <div class="header-item">
+     ${totalPuntos} pts
+     </div>
     </div>
     <div class="logo"></div>
     <div class="saludo">
