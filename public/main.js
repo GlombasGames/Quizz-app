da65ae61055ca817568312b5cfa48ee1e8c15321
Victor Glomba <victorglomba@gmail.com>
2025-05-21 17:23:52 -0300

    2

--- public/main.js
+++ public/main.js
@@ -337,7 +337,7 @@ function renderPrincipal() {
 
   app.innerHTML = `
   <div class="ppal">
-  <img src="./assets/trivian.png" >
+  <div class="trivian"></div>
   <button class="btn-jugar" onclick="renderMenu()" tabindex="0">
   <button class="btn-otras-trivias" onclick="renderMenu()" tabindex="0">
   <button class="btn-nosotros" onclick="renderMenu()" tabindex="0">
