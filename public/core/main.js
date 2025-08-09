a09f5bd6402e2a3638140ea32c653794edf15013
Victor Glomba <victorglomba@gmail.com>
2025-08-09 13:23:30 -0300

    a

--- public/core/main.js
+++ public/core/main.js
@@ -352,8 +352,7 @@ async function iniciarNotificaciones() {
 const app = document.getElementById('app');
 //Cambio la vista segun si es Android o no
 app.style.marginLeft =  window.innerWidth > 600 ? '25%' : '0';
-app.style.maxWidth =  window.innerWidth > 600 ? '50%' : '90%';
-app.style.width =  window.innerWidth > 600 ? '50%' : '90%';
+app.style.flex =  window.innerWidth > 600 ? '0.5' : '1';
 
 //creare esto solo para ver si se modifica
 const variablePrueba = 'Hola Mundo';
